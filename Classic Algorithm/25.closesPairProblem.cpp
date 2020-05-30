45f6b60c6a99431a5b1757fc82b19ffc3d89709c
aashan007 <sarathchandran1995@gmail.com>
2020-05-30 21:16:19 +0530

    closest Pair Problem

---
+++ Classic Algorithm/25.closesPairProblem.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<pair<int,int>> lists;
+    while(n--){
+        int a,b;
+        cin>>a>>b;
+        pair<int,int> p(a,b);
+        lists.push_back(p);
+    }
+    int distance =INT_MAX;
+    for(int i=0;i<lists.size()-1;i++){
+        int x1 = lists[i].first;
+        int y1 = lists[i].second;
+        for(int j=i+1;j<lists.size();j++){
+            int x2=lists[j].first;
+            int y2=lists[j].second;
+            int tempDistance = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+            distance = min(distance,tempDistance);
+        }
+    }
+    cout<<"Shortest Distance : "<<distance; 
+}
